84a77606ab74c7cbd8353451b70111cc1f6bb229
Heriniaina <irdnarheriniaina@gmail.com>
2026-02-12 06:37:26 +0300

    first test

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ public class App
 {
     public static void main(String[] args)
     {
-        byte a[] = {1, 1, 1, 1};
-        byte b[] = {0, 0, 1, 1};
+        byte a[] = {1, 1, 1, 1, 0};
+        byte b[] = {   1, 0, 1, 1};
         
         for(byte c: mg.hr.BinaryMath._addBinary(a, b, 3))
            System.out.print(c);
---
+++ test/Test_Binary.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class Test_Binary
+{
+    @BeforeAll
+    public static void setUpBeforeClass() 
+    {
+        System.out.println("before all");
+    }
+
+    @Test
+    public void _test_toBinary()
+    {
+        System.out.println("test");
+    }
+}

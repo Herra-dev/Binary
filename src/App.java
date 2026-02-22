5e8330e412344ab21d5b1c5b015cddd6afbfaa99
Heriniaina <irdnarheriniaina@gmail.com>
2026-02-22 23:15:52 +0300

    correction

--- src/App.java
+++ src/App.java
@@ -2,11 +2,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        byte[] _bit1 = null;
-
         try {
-        //     // _bit1 = mg.hr.Binary._toBinaryFloat(-1027.625, mg.hr.enumeration.FloatPrecision._HALF_PRECISION);
-        //     _bit1 = mg.hr.Binary._toBinarySignedInteger(-1, 5);
         byte[] _bit = mg.hr.Binary._toBinaryUnsignedInteger(15, 4);
         byte[] _bit2 = mg.hr.Binary._toBinaryUnsignedInteger(3, 1);
         byte[] _bit3 = mg.hr.BinaryMath._addBinary(_bit, _bit2);
@@ -14,7 +10,5 @@ public class App
         } catch (mg.hr.exception.NotAnIntegerException | mg.hr.exception.BinaryException  | mg.hr.exception.NotAnUnsignedInteger e) {
             e.printStackTrace();
         }
-
-        //mg.hr.Binary._displayBinaryNumber(_bit1);
     }
 }

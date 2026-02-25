fe601ca8c863d878a7c204861846863cd98ada70
Heriniaina <irdnarheriniaina@gmail.com>
2026-02-26 00:49:41 +0300

    correction of function _toDecimalFloat

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        byte[] _bit = {1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        byte[] _bit = mg.hr.Binary._toBinaryFloat(-1027.0000000625, mg.hr.enumeration.FloatPrecision._OCTUPLE_PRECISION);
         mg.hr.Binary._displayBinaryNumber(_bit);
         System.out.println(mg.hr.decimal.Decimal._toDecimalFloat(_bit));
     }

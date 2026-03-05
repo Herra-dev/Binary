a54097da95e298ca2ed470c9da952068c238b003
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-05 23:22:16 +0300

    documentation

--- test/mg/hr/Test_BinaryMath.java
+++ test/mg/hr/Test_BinaryMath.java
@@ -1,39 +1,38 @@
 package test.mg.hr;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
 import mg.hr.exception.NotABinaryNumberException;
 
+
 public class Test_BinaryMath
 {
     @Test
-    public void _test_addBinary()
+    public void _test_addBinaryNoBitNumber()
     {
-        int length = 8;
-        byte _tab[] = null;
-        byte _tab1[] = null;
+        try {
+            byte[] _bit00_ = {0, 0, 0, 0, 0, 1};
+            byte[] _bit01_ = {0, 0, 0, 0, 0, 1};
+            byte[] _excepted0 = {0, 0, 0, 0, 1, 0};
+            assertArrayEquals(_excepted0, mg.hr.BinaryMath._addBinary(_bit00_, _bit01_));
+            _bit00_ = null;
+            _bit01_ = null;
+            _excepted0 = null;
 
-        try 
-        {
-            _tab = mg.hr.Binary.toBinary(5, length);
-            _tab1 = mg.hr.Binary.toBinary(10, length);
-        } 
-        catch (Exception e) 
-        {
+            byte[] _bit10_ = {0, 0, 0, 0, 1, 1};
+            byte[] _bit11_ = {0, 0, 0, 0, 0, 1};
+            byte[] _excepted1 = {0, 0, 0, 1, 0, 0};
+            assertArrayEquals(_excepted1, mg.hr.BinaryMath._addBinary(_bit10_, _bit11_));
+            _bit00_ = null;
+            _bit01_ = null;
+            _excepted1 = null;
+
+
+
+        } catch (NotABinaryNumberException e) {
             e.printStackTrace();
-        }
-
-        byte _excepted[] = {0, 0, 0, 0, 1, 1, 1, 1};
-        
-        for(int i = 0; i < 8; i++)
-        {
-            try {
-                assertEquals(_excepted[i], mg.hr.BinaryMath._addBinary(_tab, _tab1, length)[i]);
-            } catch (NotABinaryNumberException e) {
-                e.printStackTrace();
-            }
         }
     }
 }

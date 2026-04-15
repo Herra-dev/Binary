703a6b961b2c78110fe503eb0ac3cf8115278c80
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-15 15:08:18 +0300

    wip

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
             byte[] binaryNumber;
             try {
-                binaryNumber = Binary.toBinary(15);
+                binaryNumber = Binary.toBinary(0.3);
                 Binary._displayBinaryNumber(binaryNumber);
             } catch (BinaryException e) {
                 e.printStackTrace();

a4962b88b58314296f071671c91ad4d5858dae1a
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-15 14:48:06 +0300

    wip: :

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
+import mg.hr.Binary;
+import mg.hr.exception.BinaryException;
 
 public class App {
     public static void main(String[] args) {
-                    
-    }
+            byte[] binaryNumber;
+            try {
+                binaryNumber = Binary.toBinary(15);
+                Binary._displayBinaryNumber(binaryNumber);
+            } catch (BinaryException e) {
+                e.printStackTrace();
+            }
+    }   
 }

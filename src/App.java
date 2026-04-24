8e3c1820d8b5eea3c8cbaa3615b3e893cbf8c9f1
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-24 13:59:47 +0300

    thinking to add UI

--- src/App.java
+++ src/App.java
@@ -1,13 +1,23 @@
 import mg.hr.Binary;
+import mg.hr.BinaryMath;
 import mg.hr.exception.BinaryException;
+import mg.hr.exception.NotABinaryNumberException;
 
 public class App {
     public static void main(String[] args) {
             byte[] binaryNumber;
+            byte[] binaryNumber1;
+            byte[] result;
             try {
-                binaryNumber = Binary.toBinary(0.000199999001);
+                binaryNumber = Binary.toBinary(1);
+                binaryNumber1 = Binary.toBinary(14);
                 Binary._displayBinaryNumber(binaryNumber);
-            } catch (BinaryException e) {
+                Binary._displayBinaryNumber(binaryNumber1);
+                result = BinaryMath._addBinary(binaryNumber, binaryNumber1, 2);
+
+                Binary._displayBinaryNumber(result);
+
+            } catch (BinaryException | NotABinaryNumberException e) {
                 e.printStackTrace();
             }
     }   

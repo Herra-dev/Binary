1e6196528daf3259f6db81b93d75988d8885b317
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-25 06:19:33 +0300

    setting up Binary Calculator Converter window

--- src/ui/BinaryDecimalConverter.java
+++ src/ui/BinaryDecimalConverter.java
@@ -4,6 +4,12 @@ import javax.swing.JFrame;
 
 public class BinaryDecimalConverter extends JFrame {
     public BinaryDecimalConverter() {
-        
+        this.setBDCProperties();
+    }
+
+    public void setBDCProperties() {
+        this.setTitle("Binary Decimal Converter");
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }

64ac345a3f9eece5dc534c00374331bf12c9076f
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-25 09:07:42 +0300

    wip

--- src/App.java
+++ src/App.java
@@ -1,24 +1,8 @@
-import mg.hr.Binary;
-import mg.hr.BinaryMath;
-import mg.hr.exception.BinaryException;
-import mg.hr.exception.NotABinaryNumberException;
+import ui.BinaryDecimalConverter;
 
 public class App {
     public static void main(String[] args) {
-            byte[] binaryNumber;
-            byte[] binaryNumber1;
-            byte[] result;
-            try {
-                binaryNumber = Binary.toBinary(1);
-                binaryNumber1 = Binary.toBinary(14);
-                Binary._displayBinaryNumber(binaryNumber);
-                Binary._displayBinaryNumber(binaryNumber1);
-                result = BinaryMath._addBinary(binaryNumber, binaryNumber1, 2);
-
-                Binary._displayBinaryNumber(result);
-
-            } catch (BinaryException | NotABinaryNumberException e) {
-                e.printStackTrace();
-            }
+        BinaryDecimalConverter bdc = new BinaryDecimalConverter();
+        bdc.setVisible(true);
     }   
 }
--- src/ui/BinaryDecimalConverter.java
+++ src/ui/BinaryDecimalConverter.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.JFrame;
 
-public class BinaryDecimalConverter extends JFrame{
-
+public class BinaryDecimalConverter extends JFrame {
+    public BinaryDecimalConverter() {
+        
+    }
 }

e2b22dfb3c4e8c95f0141a8a59b331120f852376
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-26 22:00:40 +0300

    adding BinaryMod file: enumeration to switch between binary decimal converter and binary calculator

--- src/ui/BinaryDecimalConverter.java
+++ src/ui/BinaryDecimalConverter.java
@@ -1,17 +1,24 @@
 package ui;
 
 import javax.swing.JFrame;
+import javax.swing.plaf.DimensionUIResource;
 
 public class BinaryDecimalConverter extends JFrame {
+
     public BinaryDecimalConverter() {
         this.setBDCProperties();
     }
+
+//======================================================================================
 
     public void setBDCProperties() {
         this.setTitle("Binary Decimal Converter");
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(new DimensionUIResource(500, 500));
     }
+
+//======================================================================================
 
     
 }

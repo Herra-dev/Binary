7714fa49babef0b2e600dbb97e396d2f5dc88cad
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-26 23:04:19 +0300

    adding enum BinaryMod

---
+++ src/ui/enumeration/BinaryMod.java
@@ -0,0 +1,14 @@
+package ui.enumeration;
+
+public enum BinaryMod {
+    _BDConv("Binary Decimal Convertor", 0),
+    _BCalculator("Binary Calculator", 1);
+
+    protected String _modName;
+    protected int _modNumber;
+
+    BinaryMod(String name, int number) {
+        this._modName = name;
+        this._modNumber = number;
+    }
+}
